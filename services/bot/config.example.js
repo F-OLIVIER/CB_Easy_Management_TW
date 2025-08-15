c8bc4d95b01d4455cbbc6a233692d75a14fb06b7
folivier <fabien.olivier2@gmail.com>
2025-08-15 04:12:55 +0200

    reglage

---
+++ services/bot/config.example.js
@@ -0,0 +1,17 @@
+// ╭───────────────────────────────────────────────╮
+// │                   Constante                   │
+// ╰───────────────────────────────────────────────╯
+export const adressdb = "../database/databaseGvG.db";
+export const siteInternet = "http://localhost:8080";
+export const store_app_android = "https://play.google.com/store/games?gl=FR";
+export const store_app_ios = "Not currently available";
+
+// ID utilisateur Discord autorisé a utilisé les commandes pour gérer les admin du site internet
+export const ListAdmin = ["0123456789"];
+export const listUserBan = [];
+
+// Config bot de test
+export const discordTest_id = "0123456789";
+export const discordTest_groupAdminForum = "0123456789";
+export const discordTest_chanForum = "0123456789";
+export const discordTest_chanDM = "0123456789";
