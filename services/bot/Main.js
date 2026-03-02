d277c21cd2f1de1601cc404d1cd6bb6fde17935b
folivier <fabien.olivier2@gmail.com>
2026-03-02 10:12:43 +0100

    update DiscordJS

--- services/bot/Main.js
+++ services/bot/Main.js
@@ -33,7 +33,7 @@ client.on("warn", (warning) => {
 // --------------------------------------------------------------
 
 // definition des chan utilisé par le bot
-client.on("ready", async () => {
+client.on("clientReady", async () => {
   console.log(`╭─────────────────────────────────────────────────╮
 │         Bot starting up, please wait ...        │
 │─────────────────────────────────────────────────│`);
